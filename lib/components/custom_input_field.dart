e07ccd2f5d0967841b6128310a6d055c2ec85d87
islamabououf <eng.islam.abououf@gmail.com>
2023-03-12 15:49:51 +0200

    first commit 1

---
+++ lib/components/custom_input_field.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class CustomInputField extends StatelessWidget {
+  CustomInputField({
+    Key? key,
+    required this.txt,
+    TextInputType? inputType,
+    Color? color,
+    int? maxLine,
+    int? maxLen,
+
+  }) : super(key: key);
+  String txt ;
+  TextInputType? inputType;
+  Color? color ;
+  int? maxLine, maxLen ;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(
+        left: 10,
+        right: 10,
+        bottom: 10
+      ),
+      child: TextFormField(
+        keyboardType: inputType ?? TextInputType.text,
+        decoration: InputDecoration(
+          border: const OutlineInputBorder(),
+          labelText: txt,
+          filled: true,
+          fillColor: color ?? Colors.white,
+        ),
+        minLines: 1,
+        validator: (value) {
+          if (value == null || value.isEmpty) {
+            return 'Please enter $txt';
+          }
+          return null;
+        },
+      ),
+    );
+  }
+}
